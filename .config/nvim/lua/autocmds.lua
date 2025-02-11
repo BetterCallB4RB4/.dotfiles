772c74de6ef1e119e621d05de9edb1e1765619c6
Augusto Barbadoro <a.barbadoro@crif.com>
2025-02-11 16:20:37 +0100

    add autocommand for jenkinsfile to be detected as groovy

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -20,3 +20,9 @@ vim.api.nvim_create_autocmd("BufEnter", {
     if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
   end
 })
+
+vim.cmd([[
+  augroup filetypedetect
+    autocmd BufRead,BufNewFile *.Jenkinsfile set filetype=groovy
+  augroup END
+]])
