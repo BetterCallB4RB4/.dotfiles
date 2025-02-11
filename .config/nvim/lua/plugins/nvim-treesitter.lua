54ce81817ab7bb1d1509e00d9b8d8ca3fc574c3b
Augusto Barbadoro <a.barbadoro@crif.com>
2025-02-11 16:20:22 +0100

    fix some bug con config upload in tree-sitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,36 +1,25 @@
 return {
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-          ensure_installed = { 
-			  "lua", 
-			  "vim", 
-			  "vimdoc", 
-			  "bash", 
-			  "markdown", 
-			  "go", 
-			  "terraform", 
-			  "groovy", 
-		  },
-          sync_install = true,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
+        ensure_installed = { 
+          "lua", 
+          "vim", 
+          "vimdoc", 
+          "bash", 
+          "markdown", 
+          "go", 
+          "terraform", 
+          "groovy", 
+        },
+        sync_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
     end,
-    config = function(_, opts)
-	require("nvim-treesitter.configs").setup(opts)
-	vim.cmd([[
-          augroup filetypedetect
-            autocmd BufRead,BufNewFile *.Jenkinsfile set filetype=groovy
-          augroup END
-        ]])
-	vim.cmd([[
-          augroup filetypedetect
-            autocmd BufRead,BufNewFile *.tfvars set filetype=terraform 
-          augroup END
-        ]])
-    end,
+  },
 
 }
