6950a8e043d98465ecd2e78ff4ea1ca6d8b573c7
augusto barbadoro <augusto.barbadoro244@gmail.com>
2025-02-12 21:35:38 +0100

    dummy

--- .config/nvim/lua/plugins/cmp-cmdline.lua
+++ .config/nvim/lua/plugins/cmp-cmdline.lua
@@ -1,31 +1,30 @@
-return{
-	--this plugin allow you to have a better completition and cool aspect for command like : and / 
+return {
+	--this plugin allow you to have a better completition and cool aspect for command like : and /
 	-- this will substitute wilder
 	{
-    'hrsh7th/nvim-cmp',  -- nvim-cmp plugin
-    dependencies = {
-      'hrsh7th/cmp-cmdline',  -- cmp-cmdline extension for command-line completion
-    },
-    config = function()
-      local cmp = require('cmp')
+		"hrsh7th/nvim-cmp", -- nvim-cmp plugin
+		dependencies = {
+			"hrsh7th/cmp-cmdline", -- cmp-cmdline extension for command-line completion
+		},
+		config = function()
+			local cmp = require("cmp")
 
-	  -- you can add custom setup
-      cmp.setup({
-      })
+			-- you can add custom setup
+			cmp.setup({})
 
-      -- Setup cmp-cmdline
-      require('cmp').setup.cmdline(':', {
-        sources = {
-          { name = 'cmdline' },
-        },
-      })
+			-- Setup cmp-cmdline
+			require("cmp").setup.cmdline(":", {
+				sources = {
+					{ name = "cmdline" },
+				},
+			})
 
-      -- You can also configure other cmdline sources for '/' or ':'
-      require('cmp').setup.cmdline('/', {
-        sources = {
-          { name = 'buffer' },
-        },
-      })
-    end
-  },
+			-- You can also configure other cmdline sources for '/' or ':'
+			require("cmp").setup.cmdline("/", {
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+		end,
+	},
 }
