ddfd21441b7459f0f6ba2d90ad07b396c5bd268e
Augusto Barbadoro <a.barbadoro@crif.com>
2025-03-03 16:10:41 +0100

    add obsidian plugin

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		  "epwalsh/obsidian.nvim",
+		  version = "*",  -- recommended, use latest release instead of latest commit
+		  lazy = true,
+		  ft = "markdown",
+		  dependencies = {
+			  "nvim-lua/plenary.nvim",
+			  "hrsh7th/nvim-cmp",
+			  "nvim-telescope/telescope.nvim",
+			  "nvim-treesitter/nvim-treesitter",
+		  },
+		  opts = {
+		    workspaces = {
+		      {
+			name = "personal",
+			path = "~/obsidian-notes/",
+		      },
+		    },
+		  },
+	},
+}
