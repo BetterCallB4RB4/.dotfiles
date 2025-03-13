53026aae036fbab0d870ee9d521c11ff6629e3c7
Augusto Barbadoro <a.barbadoro@crif.com>
2025-03-13 10:48:22 +0100

    change config on tmux sessionizer

--- .config/tmux/tmux-sessionizer.sh
+++ .config/tmux/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/gitOps ~/obsidian ~/gitHub ~/projects ~/scripts ~/devOps -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/gitOps ~/gitHub ~/projects ~/scripts ~/devOps -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
