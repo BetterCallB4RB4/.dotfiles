0e6288c61a3fb0df65917e799978972ebd0d558d
Augusto Barbadoro <a.barbadoro@crif.com>
2025-03-19 11:35:32 +0100

    centralize all go specific configuration

--- .config/nvim/lua/plugins/nvim-dap-go.lua
+++ .config/nvim/lua/plugins/nvim-dap-go.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"leoluz/nvim-dap-go",
-		ft = "go",
-		dependencie = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-			"theHamsta/nvim-dap-virtual-text",
-		},
-		config = function(_, opts)
-			require("dap-go").setup(opts)
-		end,
-	},
+	-- {
+	-- 	"leoluz/nvim-dap-go",
+	-- 	ft = "go",
+	-- 	dependencie = {
+	-- 		"mfussenegger/nvim-dap",
+	-- 		"nvim-neotest/nvim-nio",
+	-- 		"theHamsta/nvim-dap-virtual-text",
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("dap-go").setup(opts)
+	-- 	end,
+	-- },
 }
