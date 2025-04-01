51156d53d7e6cf48b2cf8a0e1bd9c94692472f3c
Augusto Barbadoro <a.barbadoro@crif.com>
2025-04-01 11:55:51 +0200

    refactor the code for the autocommand

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,3 +1,4 @@
+-- highlight yanked test
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
@@ -6,21 +7,23 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("VimEnter", {
+-- close nvim when nvimtree is the only window left
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+	pattern = "NvimTree_*",
 	callback = function()
-		vim.cmd(":hi LineNr guibg=none guifg=#ffffff")
+		local layout = vim.api.nvim_call_function("winlayout", {})
+		if
+			layout[1] == "leaf"
+			and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
+			and layout[3] == nil
+		then
+			vim.cmd("confirm quit")
+		end
 	end,
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
-  pattern = "NvimTree_*",
-  callback = function()
-    local layout = vim.api.nvim_call_function("winlayout", {})
-    if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
-  end
-})
-
+-- highlight jenkins ad groovy text by passing the correct filetype
 vim.cmd([[
   augroup filetypedetect
     autocmd BufRead,BufNewFile *.Jenkinsfile set filetype=groovy
