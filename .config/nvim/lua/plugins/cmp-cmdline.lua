56960c07ef45b65802f51a6dcbcc710325d94d3e
augusto barbadoro <augusto.barbadoro244@gmail.com>
2025-04-07 15:58:11 +0200

    remove cmp command line

--- .config/nvim/lua/plugins/cmp-cmdline.lua
+++ .config/nvim/lua/plugins/cmp-cmdline.lua
@@ -1,25 +1,25 @@
--- this is still not compliting the with fuzzy as i want
 return {
-	-- this plugin allow you to have a better completition and cool aspect for command like : and /
-	-- this will substitute wilder
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-cmdline",
-		},
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup({})
-			require("cmp").setup.cmdline(":", {
-				sources = {
-					{ name = "cmdline" },
-				},
-			})
-			require("cmp").setup.cmdline("/", {
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"hrsh7th/cmp-cmdline",
+	-- 	cmp.setup.cmdline(":", {
+	-- 		mapping = cmp.mapping.preset.cmdline(),
+	-- 		sources = cmp.config.sources({
+	-- 			{ name = "path" },
+	-- 		}, {
+	-- 			{
+	-- 				name = "cmdline",
+	-- 				option = {
+	-- 					ignore_cmds = { "Man", "!" },
+	-- 				},
+	-- 			},
+	-- 		}),
+	-- 	}),
+	--
+	-- 	cmp.setup.cmdline("/", {
+	-- 		mapping = cmp.mapping.preset.cmdline(),
+	-- 		sources = {
+	-- 			{ name = "buffer" },
+	-- 		},
+	-- 	}),
+	-- },
 }
