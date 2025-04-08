8c4f71b5b0384ce09e05af060eba15341b381c39
Augusto Barbadoro <a.barbadoro@crif.com>
2025-04-08 18:08:20 +0200

    add obsidian and a markdown render

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -13,7 +13,7 @@ return {
 			workspaces = {
 				{
 					name = "personal",
-					path = "~/obsidian/obsidian-notes",
+					path = "~/gitHub/obsidian-notes",
 				},
 			},
 		},
---
+++ .config/nvim/lua/plugins/renderMD.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"echasnovski/mini.nvim",
+			"saghen/blink.cmp",
+		},
+
+		config = function()
+			require("obsidian").setup({
+				ui = { enable = true },
+			})
+			require("render-markdown").setup({
+				completions = { blink = { enabled = true } },
+			})
+		end,
+	},
+}
