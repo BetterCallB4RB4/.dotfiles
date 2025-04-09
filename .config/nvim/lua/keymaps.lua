e96c2e8d924c0a22030dcfbada172b6dbcdf16da
Augusto Barbadoro <a.barbadoro@crif.com>
2025-04-09 09:31:36 +0200

    sync repo

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -17,3 +17,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Indent selected text to the left and right
 vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
 vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
+
+-- never lost last buffer after a copy over
+vim.keymap.set("x", "p", [["_dP]])
