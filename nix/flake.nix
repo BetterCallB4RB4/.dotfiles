868f77a3137777a1440e899b63d6ead79c714236
Augusto <augusto.barbadoro244@gmail.com>
2025-12-14 12:58:57 +0100

    add wsl config in the flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -64,7 +64,7 @@
           system = "x86_64-linux";
           machine = ./machines/wsl;
           username = "augusto";
-          desktop = null; # WSL usually doesn’t need a desktop profile
+          desktop = personal;
         };
       };
 
