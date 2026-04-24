80f12a5983cbaccf6b96cd22d91a443090b332aa
Augusto Barbadoro <36622711+BetterCallB4RB4@users.noreply.github.com>
2026-04-24 11:20:48 +0200

    Refactor nvim-treesitter configuration for clarity

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,24 +1,31 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				ensure_installed = {
-					"lua",
-					"vim",
-					"vimdoc",
-					"bash",
-					"markdown",
-					"go",
-					"terraform",
-					"groovy",
-				},
-				sync_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    main = "nvim-treesitter.configs",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      local install_dir = vim.fn.stdpath("data") .. "/treesitter"
+
+      vim.opt.runtimepath:append(install_dir)
+
+      require("nvim-treesitter.configs").setup({
+        parser_install_dir = install_dir,
+        ensure_installed = {
+          "lua",
+          "vim",
+          "vimdoc",
+          "bash",
+          "markdown",
+          "go",
+          "terraform",
+          "groovy",
+        },
+        sync_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }
